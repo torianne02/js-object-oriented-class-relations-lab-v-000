70459fe09db79e5bd91474f7c7d9419f021c8bf2
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 18:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ class Driver {
 
     store.drivers.push(this);
   }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId == this.id;
+    });
+  }
+
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger();
+    });
+  }
 }
